10da21cddc713889baa4547858b3b520679efb28
VinuKarthek <vinukarthek@gmail.com>
2016-03-22 16:34:01 +0530

    Update UDP Server.py

--- UDP Server.py
+++ UDP Server.py
@@ -1,4 +1,4 @@
-import SocketServer
+"""import SocketServer
 
 class MyUDPHandler(SocketServer.BaseRequestHandler):
    def handle(self):
@@ -11,4 +11,15 @@ class MyUDPHandler(SocketServer.BaseRequestHandler):
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
     server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
-    server.serve_forever()
+    server.serve_forever()"""
+
+import socket
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, # Internet
+ socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
