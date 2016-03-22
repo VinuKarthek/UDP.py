5e3f75609cc98ec01c39406f4d96f3aab0d18c15
VinuKarthek <vinukarthek@gmail.com>
2016-03-22 10:59:54 +0530

    Create UDP Server.py

---
+++ UDP Server.py
@@ -0,0 +1,14 @@
+import SocketServer
+
+class MyUDPHandler(SocketServer.BaseRequestHandler):
+   def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print "{} wrote:".format(self.client_address[0])
+        print data
+        socket.sendto(data.upper(), self.client_address)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
+    server.serve_forever()
