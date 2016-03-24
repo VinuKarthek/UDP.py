4ae597acd2cfaaf1cfd6b6789bc5e0b5e2b8099b
Vinu Karthek <vinukarthek@gmail.com>
2016-03-24 10:53:03 +0530

    Update UDP Client.py

--- UDP Client.py
+++ UDP Client.py
@@ -16,11 +16,20 @@ print "Sent:     {}".format(data)
 print "Received: {}".format(received)"""
 
 import socket
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+import time
+
+UDP_IP = "localhost"
+UDP_PORT = 5454
 MESSAGE = "Hello, World!"
+
 print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
-sock = socket.socket(socket.AF_INET, # Internet
-socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+Clientsocket = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+while 1:
+    Clientsocket.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    print "sent: "+MESSAGE
+    time.sleep(1) # Delay to reduce overloading
