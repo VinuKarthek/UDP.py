0780173e2c6234605fd5d41e417f8a6f08202be1
Vinu Karthek <vinukarthek@gmail.com>
2016-03-28 22:36:26 +0530

    Update UDP Client.py

--- UDP Client.py
+++ UDP Client.py
@@ -1,20 +1,3 @@
-"""import socket
-import sys
-
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(data + "\n", (HOST, PORT))
-received = sock.recv(1024)
-
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)"""
-
 import socket
 import time
 
@@ -22,14 +5,14 @@ UDP_IP = "localhost"
 UDP_PORT = 5454
 MESSAGE = "Hello, World!"
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+print ("message:", MESSAGE)
 
 Clientsocket = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
 while 1:
-    Clientsocket.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    print "sent: "+MESSAGE
+    Clientsocket.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+    print ("sent: "+MESSAGE)
     time.sleep(1) # Delay to reduce overloading
