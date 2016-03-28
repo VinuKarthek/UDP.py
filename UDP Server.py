6319e0d9680ff93dc326eca7e71fb92d3fc2d450
Vinu Karthek <vinukarthek@gmail.com>
2016-03-28 22:37:23 +0530

    Update UDP Server.py

--- UDP Server.py
+++ UDP Server.py
@@ -1,18 +1,3 @@
-"""import SocketServer
-
-class MyUDPHandler(SocketServer.BaseRequestHandler):
-   def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        print data
-        socket.sendto(data.upper(), self.client_address)
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
-    server.serve_forever()"""
-
 import socket
 
 UDP_IP = "localhost"
@@ -23,6 +8,5 @@ ServerSocket = socket.socket(socket.AF_INET, # Internet
 ServerSocket.bind((UDP_IP, UDP_PORT))
 
 while 1:
-    print "Waiting for data:"
-    print "Received: "+ServerSocket.recv(30) # buffer size is 30 bytes
+    print ("Received: "+str(ServerSocket.recv(30))) # buffer size is 30 bytes
     
